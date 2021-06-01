f68ed441436ef5850e7295c259dd0bd44d125cb2
Marco Schmitz <inf3067@hs-worms.de>
2021-06-01 02:12:53 +0200

    Einzelne Angebote in Card Widgets packen

--- myapp/lib/widgets/offer_tile.dart
+++ myapp/lib/widgets/offer_tile.dart
@@ -11,24 +11,24 @@ class OfferTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(4),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
-        borderRadius: BorderRadius.circular(6),
-        color: Colors.white70,
-        image: DecorationImage(
-          image: AssetImage(availableOffers[offerID].offerImage),
-          fit: BoxFit.fitWidth,
+    return Card(
+      child: Container(
+        margin: EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: Colors.white70,
+          image: DecorationImage(
+            image: AssetImage(availableOffers[offerID].offerImage),
+            fit: BoxFit.fitWidth,
+          ),
         ),
-      ),
-      child: Column(
-        children: [
-          Text(availableOffers[offerID].name),
-          Expanded(child: SizedBox()),
-          Text(availableOffers[offerID].productPrice.toString() +
-              '€') // Preis aus dem Listeneintrag holen und €-Zeichen anhängen
-        ],
+        child: Column(
+          children: [
+            Text(availableOffers[offerID].name),
+            Expanded(child: SizedBox()),
+            Text(availableOffers[offerID].productPrice.toString() +
+                '€') // Preis aus dem Listeneintrag holen und €-Zeichen anhängen
+          ],
+        ),
       ),
     );
   }
