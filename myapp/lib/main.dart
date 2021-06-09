d985b718a39fedb44aa4009766c486ad38cb191f
Marco Schmitz <inf3067@hs-worms.de>
2021-06-09 12:37:19 +0200

    Lightmode als Standard setzen, Titel der App anpassen

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
           final themeProvider = Provider.of<ThemeProvider>(context);
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'FlutterDemo',
+            title: 'Smartbuy',
             themeMode: themeProvider.themeMode,
             theme: MyThemes.lightMode,
             darkTheme: MyThemes.darkMode,
