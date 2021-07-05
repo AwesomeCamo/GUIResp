14b3c4ef0766ddd2a0b99f5d55a68452826dd1a2
Robin <robin@>
2021-07-05 11:28:03 +0200

    improve contact_page

--- myapp/lib/contact_page.dart
+++ myapp/lib/contact_page.dart
@@ -19,9 +19,14 @@ class _ContactPageState extends State<ContactPage> {
                 Text(
                   'Kontakt:',
                   style: TextStyle(fontSize: 48),),
-                Text(
-                    '\nBetreiber Addresse\n',
-                    style: TextStyle(fontSize:  14 + constraints.maxWidth / 100),),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 75.0, top: 25.0),
+                  child: Text(
+                      'Betreiber Addresse',
+                      style: TextStyle(fontSize:  14 + constraints.maxWidth / 100),)),
+                  Text(
+                    'Rechtliche Hinweise:',
+                    style: TextStyle(fontSize: 36),),
                 Text(
                   'Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und nach bestem Gewissen erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.',
                   style: TextStyle(fontSize: 6 + constraints.maxWidth / 100),
